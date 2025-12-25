5449d688271ccef21064eccd5dea001efd9f4e9e
AlexRashidov <rashidov.sasha@yandex.ru>
2025-12-25 18:45:03 +0700

    add resume_api.js

---
+++ resume_api.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const PORT = 3000;
+
+const db = new sqlite3.Database('CV_API.db', (err) => {
+    if (err) {
+        console.error('Ошибка подключения к базе данных:', err);
+    } else {
+        console.log('Подключено к SQLite базе данных');
+    }
+});
+
+app.get('/resume/list', function (req,res) {
+    db.all("SELECT * FROM resume", [], (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
+
+// Закрытие соединения с БД при завершении
+process.on('SIGINT', () => {
+    db.close();
+    process.exit();
+});
